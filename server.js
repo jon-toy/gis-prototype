d13d0734404c95335e88bab01c0f13217a8f431d
Jon Toy <jonathon.toy@gmail.com>
2018-04-06 00:09:05 -0700

    Test with Sample GIS data

--- server.js
+++ server.js
@@ -2,8 +2,10 @@ const express = require('express');
 const app = express();
 
 app.get('/', function (req, res) {
-  res.sendFile(__dirname + '/index.html');
+  res.sendFile(__dirname + '/public/index.html');
 })
+
+app.use(express.static('public'));
 
 app.listen(80, function () {
   console.log('Test app listening on port 80!');
