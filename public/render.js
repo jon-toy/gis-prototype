ed512a03e6e35fd66f099164e1e033d27945c92d
Jon Toy <jonathon.toy@gmail.com>
2018-04-24 22:42:42 -0700

    UI changes and JS code cleanup

--- public/render.js
+++ public/render.js
@@ -1,145 +1,33 @@
-var map, GeoMarker;
-var geo_json_urls = [];
-var shown_parcel_on_startup = false;
+const FEATURE_LABEL_VISIBLE_ZOOM_THRESHOLD = 16; // Hide markers below this threshold
+
+var map, GeoMarker; // Google Maps API objects
+var geo_json_urls = []; // URLs for all the GeoJSON objects after listing the results from the server.
+						// Global so we can access it in callbacks
 var all_features = []; // Unreliable on page load. Used for calls to action after page render
-var parcel_num_markers = [];
-const MARKER_ZOOM_THRESHOLD = 16;
+var parcel_num_markers = []; // Store references to all markers currently on the page so we can manipulate en masse
 
-function initPage()
-{
-	// Get GeoJSON list from Data API
-	$.get("/get-maps", function(data, status)
-	{
-		geo_json_urls = [];
-
-		for ( var i = 0; i < data.body.files.length; i++ ) 
-		{
-			geo_json_urls.push(data.host + "/maps/" + data.body.files[i]);
-		}
-
-		initMap(null);
-		
-		// Load the GeoJSONs
-		for ( var i = 0; i < geo_json_urls.length; i++ )
-		{
-			$.getJSON(geo_json_urls[i], function (data) 
-			{
-				var selected_feature = null;
-				 var features = map.data.addGeoJson(data);
-				 $.each( features, function( index, feature ) {
-					  if ( feature.getProperty('PARCEL_NUM') == getParam('parcel') )
-						{
-							viewParcel(feature);
-							shown_parcel_on_startup = true;
-							selected_feature = feature;
-						}
-					});
-				all_features = all_features.concat(features);
-				
-				if ( selected_feature != null ) 
-				{
-					selectFeature(selected_feature);
-				}
-			});
-		}		
-	});
-}
-
-function labelFeature(feature)
-{
-	if ( map.getZoom() < MARKER_ZOOM_THRESHOLD ) return; // Don't show labels when zoomed out so much
-	// Place a marker on there
-	var geom = feature.getGeometry();
-	var poly = new google.maps.Polygon({
-		paths: geom.getAt(0).getArray(),
-	});
-
-	var center = polygonCenter(poly);
-
-	var marker = new google.maps.Marker({
-	  position: center,
-	  map: map,
-	  label: feature.getProperty('PARCEL_NUM'),
-	  icon: "blank.png"
-	});
-
-	parcel_num_markers.push(marker);
-}
-
-function selectFeature(selected_feature)
-{
-	// Style and color the selected feature
-	map.data.overrideStyle(selected_feature, {strokeWeight: 8, fillColor:'green', strokeColor:'green'});
-
-	labelFeature(selected_feature);
-
-	var geom = selected_feature.getGeometry();
-	var poly = new google.maps.Polygon({
-		paths: geom.getAt(0).getArray(),
-	});
-	var center = polygonCenter(poly);
-	map.panTo(center);
-}
-
-function onSearchByParcelNo()
-{
-	event.preventDefault();
-	var parcel_num = document.getElementById("search-by-parcel-number").value;
-	if ( parcel_num == null || parcel_num.length <= 0 ) return;
-
-	for ( var i = 0; i < all_features.length; i++ ) 
-	{
-		var feature = all_features[i];
-		
-		if ( feature.getProperty('PARCEL_NUM') == parcel_num )
-		{
-			viewParcel(feature);
-			shown_parcel_on_startup = true;
-
-			return;
-		}
-	}
-}
-
-function goToLatLon()
-{
-	var lat_val = parseFloat(document.getElementById("lat").value);
-	var lon_val = parseFloat(document.getElementById("lon").value);
-
-	var my_lat_lon = new google.maps.LatLng(lat_val, lon_val);
-
-	map.panTo(my_lat_lon);
-	map.setZoom(18);	
-	
-	$.each( all_features, function( index, feature ) {
-		var geom = feature.getGeometry();
-		var poly = new google.maps.Polygon({
-			paths: geom.getAt(0).getArray(),
-		});
-		if ( google.maps.geometry.poly.containsLocation(my_lat_lon, poly) == true )
-		{
-			if ( document.getElementById("view-parcel-for-lat-lon").checked == true )
-			{
-				// Show the info
-				viewParcel(feature);
-			}
-			else
-			{
-				// Just highlight the nearest parcel
-				map.data.revertStyle();
-				map.data.overrideStyle(feature, {strokeWeight: 8, fillColor:'green', strokeColor:'green'});
-			}
-		}
-	});
-
-	var marker = new google.maps.Marker({
-		position: my_lat_lon,
-		map: map
-	  });
-}
-
-// Submit Feedback
 $(document).ready(function() {
+	initFeedback();
+  });
+
+/**
+ * Helper function to get URL Parameters
+ * @param {*} name of the parameter
+ */
+function getUrlParam(name)
+{
+	var results = new RegExp('[\?&]' + name + '=([^&#]*)').exec(window.location.href);
+
+	if ( results == null ) return null;
+
+	return results[1] || 0;
+}
+
+/**
+ * Set up the reCaptcha listeners
+ */
+function initFeedback()
+{
 	window.verifyRecaptchaCallback = function (response) {
         $('input[data-recaptcha]').val(response).trigger('change')
     }
@@ -168,41 +56,104 @@ $(document).ready(function() {
 	  //Very important line, it disable the page refresh.
 	  return false;
 	});
-  });
+}  
 
+/**
+ * Called by Google Maps API after its loaded. Makes a call out to the server to get a list
+ * of the GeoJSONs, then calls out to the data API to grab the GeoJSONs individually and
+ * load them asynchonously
+ */
+function initPage()
+{
+	// Get GeoJSON list
+	$.get("/get-maps", function(data, status)
+	{
+		geo_json_urls = [];
+
+		for ( var i = 0; i < data.body.files.length; i++ ) 
+		{
+			geo_json_urls.push(data.host + "/maps/" + data.body.files[i]);
+		}
+
+		// Create the Map object
+		initMap(null);
+		
+		// Load the GeoJSONs
+		for ( var i = 0; i < geo_json_urls.length; i++ )
+		{
+			$.getJSON(geo_json_urls[i], function (data) 
+			{
+				var selected_feature = null;
+				 var features = map.data.addGeoJson(data);
+				 $.each( features, function( index, feature ) {
+					  if ( feature.getProperty('PARCEL_NUM') == getUrlParam('parcel') )
+						{
+							showFeature(feature);
+							selected_feature = feature;
+						}
+					});
+				all_features = all_features.concat(features);
+				
+				if ( selected_feature != null ) 
+				{
+					selectFeature(selected_feature);
+				}
+			});
+		}		
+	});
+}
+
+/**
+ * Create the map object and set up the listeners
+ * @param {*} my_lat_lon 
+ */
 function initMap(my_lat_lon) 
 {
-	if ( my_lat_lon == null ) my_lat_lon = new google.maps.LatLng(33.83199129270437, -109.120958336746);
+	if ( my_lat_lon == null ) my_lat_lon = new google.maps.LatLng(33.83199129270437, -109.120958336746); // Starting position
+	;
 
 	map = new google.maps.Map(document.getElementById('map'), {
 	  center: my_lat_lon,
-	  zoom: MARKER_ZOOM_THRESHOLD,
+	  zoom: FEATURE_LABEL_VISIBLE_ZOOM_THRESHOLD,
 	  fullscreenControl: false
 	});
 
-	map.data.addListener('click', function(event) 
-	{			
-		viewParcel(event.feature);
-	});
-
-	setColors();
-
+	// Highlight the parcels
 	map.data.addListener('mouseover', function(event) {
-	  map.data.overrideStyle(event.feature, {strokeWeight: 8, fillColor:'green', strokeColor:'green'});
-	  displayCoordinates(event.latLng);
-	  displayParcel(event.feature);
-	});
+		map.data.overrideStyle(event.feature, {strokeWeight: 8, fillColor:'green', strokeColor:'green'});
+		displayCoordinates(event.latLng);
+		displayParcel(event.feature);
+	  });
 
 	map.data.addListener('mouseout', function(event) {
-	  map.data.revertStyle();
+		map.data.revertStyle();
 	});
 	
+
+	// Show modal on click
+	map.data.addListener('click', function(event) 
+	{			
+		showFeature(event.feature);
+	});
+
+	// Set colors
+	map.data.setStyle(function(feature) {
+		var color = 'gray';
+		return /** @type {google.maps.Data.StyleOptions} */({
+		  fillColor: color,
+		  strokeColor: color,
+		  strokeWeight: 2
+		});
+	  });
+
+	// Populate the Lat Lon. Separate from the mouseover so we keep track outside the parcels
 	google.maps.event.addListener(map, 'mousemove', function (event) {
 	  displayCoordinates(event.latLng);               
 	});
 
+	// Wipe out the labels after we zoom out enough so it doesn't clutter the map
 	map.addListener('zoom_changed', function() {
-		if ( map.getZoom() < MARKER_ZOOM_THRESHOLD )
+		if ( map.getZoom() < FEATURE_LABEL_VISIBLE_ZOOM_THRESHOLD )
 		{
 			// Wipe markers
 			for ( var i = 0; i < parcel_num_markers.length; i++ )
@@ -232,6 +183,10 @@ function initMap(my_lat_lon)
 			});
 	}
 
+	/**
+	 * Display the Lat/Lon on the bottom bar
+	 * @param {*} pnt 
+	 */
 	function displayCoordinates(pnt) 
 	{
 		var lat = pnt.lat();
@@ -241,26 +196,22 @@ function initMap(my_lat_lon)
 		document.getElementById("latlon-display").innerHTML = lat + ", " + lng;
 	}
 
+	/**
+	 * Display the Parcel Number on the bottom bar
+	 * @param {*} feature 
+	 */
 	function displayParcel(feature) 
 	{
 		document.getElementById("parcel-num-display").innerHTML = "Parcel Number: " + feature.getProperty('PARCEL_NUM');
 	}
 }
 
-function setColors()
-{
-	// Set colors
-	map.data.setStyle(function(feature) {
-	  var color = 'gray';
-	  return /** @type {google.maps.Data.StyleOptions} */({
-		fillColor: color,
-		strokeColor: color,
-		strokeWeight: 2
-	  });
-	});
-}
-
-function viewParcel(feature)
+/**
+ * Parse out properties from the feature, place those properties into the Parcel Modal,
+ * and show it.
+ * @param {*} feature 
+ */
+function showFeature(feature)
 {	  
 	map.data.revertStyle();
 	map.data.overrideStyle(feature, {strokeWeight: 8, fillColor:'green', strokeColor:'green'});
@@ -274,7 +225,7 @@ function viewParcel(feature)
 	document.getElementById("parcelModalLabel").innerHTML = "Parcel " + parcel;
 
 	renderProperty(info_box, "Parcel", parcel);
-	if ( account_number ) renderProperty(info_box, "Eagle Web", '<a target="_blank" href="http://eagleweb.assessor.co.apache.az.us/assessor/taxweb/account.jsp?guest=true&accountNum=' + account_number + '">' + account_number + '</a>')
+	if ( account_number ) renderProperty(info_box, "Parcel Information", '<a target="_blank" href="http://eagleweb.assessor.co.apache.az.us/assessor/taxweb/account.jsp?guest=true&accountNum=' + account_number + '">' + account_number + '</a>')
 	renderProperty(info_box, "Description", feature.getProperty('DESCRIPTIO'));
 	renderProperty(info_box, "FCV", feature.getProperty('FCV'));
 	renderProperty(info_box, "Legal", feature.getProperty('LEGAL'));
@@ -294,61 +245,162 @@ function viewParcel(feature)
 	$("#parcelModal").modal("show");
 
 	selectFeature(feature);
+
+	function renderProperty(container, title, content)
+	{
+		if ( content == null ) return;
+
+		var row = document.createElement('div');
+		row.className = "row p-2";
+
+		var title_container = document.createElement('div');
+		title_container.className = 'col-3';
+		title_container.innerHTML = '<b>' + title + '</b>';
+		row.appendChild(title_container);
+
+		var content_container = document.createElement('div');
+		content_container.className = 'col-9';
+		content_container.innerHTML = content;
+		row.appendChild(content_container);
+
+		container.appendChild(row);
+	}
 }
 
-function renderProperty(container, title, content)
+/**
+ * Draw a label on the map. The contents of the label are the feature's PARCEL_NUM property
+ * @param {*} feature 
+ */
+function labelFeature(feature)
 {
-	if ( content == null ) return;
+	if ( map.getZoom() < FEATURE_LABEL_VISIBLE_ZOOM_THRESHOLD ) return; // Don't show labels when zoomed out so much
+	// Place a marker on there
+	var geom = feature.getGeometry();
+	var poly = new google.maps.Polygon({
+		paths: geom.getAt(0).getArray(),
+	});
 
-	var row = document.createElement('div');
-	row.className = "row p-2";
+	var center = getPolygonCenter(poly);
 
-	var title_container = document.createElement('div');
-	title_container.className = 'col-3';
-	title_container.innerHTML = '<b>' + title + '</b>';
-	row.appendChild(title_container);
+	var marker = new google.maps.Marker({
+	  position: center,
+	  map: map,
+	  label: feature.getProperty('PARCEL_NUM'),
+	  icon: "blank.png"
+	});
 
-	var content_container = document.createElement('div');
-	content_container.className = 'col-9';
-	content_container.innerHTML = content;
-	row.appendChild(content_container);
-
-	container.appendChild(row);
+	parcel_num_markers.push(marker);
 }
 
-function getParam(name)
+/**
+ * Change the style of the feature that is selected, and pan to it
+ * @param {*} selected_feature 
+ */
+function selectFeature(selected_feature)
 {
-	var results = new RegExp('[\?&]' + name + '=([^&#]*)').exec(window.location.href);
+	// Style and color the selected feature
+	map.data.overrideStyle(selected_feature, {strokeWeight: 8, fillColor:'green', strokeColor:'green'});
 
-	if ( results == null ) return null;
+	labelFeature(selected_feature);
 
-	return results[1] || 0;
+	var geom = selected_feature.getGeometry();
+	var poly = new google.maps.Polygon({
+		paths: geom.getAt(0).getArray(),
+	});
+	var center = getPolygonCenter(poly);
+	map.panTo(center);
 }
 
-/** Function to get center of polygon.
-	Found at: https://gist.github.com/jeremejazz/9407568
-**/
-function polygonCenter(poly) {
-    var lowx,
-        highx,
-        lowy,
-        highy,
-        lats = [],
-        lngs = [],
-        vertices = poly.getPath();
+/**
+ * Get the center of a Polygon. Code found at
+ * https://gist.github.com/jeremejazz/9407568
+ * @param {*} poly 
+ */
+function getPolygonCenter(poly) 
+{
+	var lowx,
+		highx,
+		lowy,
+		highy,
+		lats = [],
+		lngs = [],
+		vertices = poly.getPath();
 
-    for(var i=0; i<vertices.length; i++) {
-      lngs.push(vertices.getAt(i).lng());
-      lats.push(vertices.getAt(i).lat());
-    }
+	for(var i=0; i<vertices.length; i++) {
+		lngs.push(vertices.getAt(i).lng());
+		lats.push(vertices.getAt(i).lat());
+	}
 
-    lats.sort();
-    lngs.sort();
-    lowx = lats[0];
-    highx = lats[vertices.length - 1];
-    lowy = lngs[0];
-    highy = lngs[vertices.length - 1];
-    center_x = lowx + ((highx-lowx) / 2);
-    center_y = lowy + ((highy - lowy) / 2);
-    return (new google.maps.LatLng(center_x, center_y));
-  }
+	lats.sort();
+	lngs.sort();
+	lowx = lats[0];
+	highx = lats[vertices.length - 1];
+	lowy = lngs[0];
+	highy = lngs[vertices.length - 1];
+	center_x = lowx + ((highx-lowx) / 2);
+	center_y = lowy + ((highy - lowy) / 2);
+	return (new google.maps.LatLng(center_x, center_y));
+}
+
+/**
+ * Handler for Search By Parcel Number box. Loops through collection of all features and
+ * checks if the value in the box matches with any Parcel Numbers (exact match).
+ */
+function onSearchByParcelNo()
+{
+	event.preventDefault();
+	var parcel_num = document.getElementById("search-by-parcel-number").value;
+	if ( parcel_num == null || parcel_num.length <= 0 ) return;
+
+	for ( var i = 0; i < all_features.length; i++ ) 
+	{
+		var feature = all_features[i];
+		
+		if ( feature.getProperty('PARCEL_NUM') == parcel_num )
+		{
+			showFeature(feature);
+
+			return;
+		}
+	}
+}
+
+/**
+ * Jump to a specific Lat/Lon on the map object
+ */
+function goToLatLon()
+{
+	var lat_val = parseFloat(document.getElementById("lat").value);
+	var lon_val = parseFloat(document.getElementById("lon").value);
+
+	var my_lat_lon = new google.maps.LatLng(lat_val, lon_val);
+
+	map.panTo(my_lat_lon);
+	map.setZoom(18);	
+	
+	$.each( all_features, function( index, feature ) {
+		var geom = feature.getGeometry();
+		var poly = new google.maps.Polygon({
+			paths: geom.getAt(0).getArray(),
+		});
+		if ( google.maps.geometry.poly.containsLocation(my_lat_lon, poly) == true )
+		{
+			if ( document.getElementById("view-parcel-for-lat-lon").checked == true )
+			{
+				// Show the info
+				showFeature(feature);
+			}
+			else
+			{
+				// Just highlight the nearest parcel
+				map.data.revertStyle();
+				map.data.overrideStyle(feature, {strokeWeight: 8, fillColor:'green', strokeColor:'green'});
+			}
+		}
+	});
+
+	var marker = new google.maps.Marker({
+		position: my_lat_lon,
+		map: map
+	  });
+}
