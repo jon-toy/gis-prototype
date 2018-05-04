40e32ae416b6460a21cc2b7e7d98f10298ef682a
Jon Toy <jonathon.toy@gmail.com>
2018-05-03 22:09:09 -0700

    Test fades for safari

--- public/render.js
+++ public/render.js
@@ -122,14 +122,14 @@ function initPage()
 
 				if ( load_completed.length == geo_json_urls.length ) 
 				{
-					$(".loading").fadeOut(1500);
+					//$(".loading").fadeOut(1500);
 					if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) 
 					{
 						// Don't fade the logo in on mobile
 					}
 					else
 					{
-						$("#logo-container").fadeIn(1500);
+						//$("#logo-container").fadeIn(1500);
 					}
 				}
 				else
